c80c3192251a03f66393f04443d8467509349d3e
Saudi Linux <SaudiLinux@users.noreply.github.com>
2025-06-26 11:18:31 +0000

    Add files via upload

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Qanas Modules Package
+Developed by Saudi Linux (SaudiLinux1@gmail.com)
+
+This package contains all the scanning modules used by Qanas.
+"""
+
+__all__ = [
+    'port_scanner',
+    'web_scanner',
+    'dns_scanner',
+    'vuln_scanner',
+    'recon_scanner'
+]
